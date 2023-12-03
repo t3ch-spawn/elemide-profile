c78458d6757d30dac0fa5b4c0cfa50dfff6b4871
Tife <boluwatife1904@gmail.com>
2023-12-03 15:35:07 +0100

    changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,11 @@
 <script>
 import Home from "./components/Home.svelte";
-import {Router, Route} from "svelte-routing"
+// import {Router, Route} from "svelte-routing"
 import About from "./components/About.svelte"
 </script>
 
 
-<Router>
-    <Route path = '/' component = {Home} />
-    <Route path = '/about' component = {About} />
-</Router>
-
+<Home/>
 
 
 <style>
